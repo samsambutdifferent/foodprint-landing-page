ccfde5c48e8f6808bd76aa5c65cb486184d02155
samsambutdifferent <sam@solutions.to>
2022-08-14 23:48:33 +0100

    initial modifications

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/frog-prints.png" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="My Carbon Food Print" />
